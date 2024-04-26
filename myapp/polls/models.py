60781586ff0e94acc97ed99718e1f9938bb248c5
JakubImianowski <kototalny@gmail.com>
2024-04-26 07:45:43 +0200

    jkhkughig

--- myapp/polls/models.py
+++ myapp/polls/models.py
@@ -7,6 +7,9 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
@@ -15,6 +18,6 @@ class Choice(models.Model):
     def __str__(self):
         return self.choice_text
 
-    
+
 
 
